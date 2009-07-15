5c906f94e49e7c49e581524676df9e8ddd87e68b
Brent Greeff <brentgreeff@brent-greeffs-macbook.local>
2009-07-15 13:45:03 +0100

    Added the attribute test helpers, missing_attrib, blank_attrib etc.

---
+++ lib/should_b/attribute_test_helper.rb
@@ -0,0 +1,28 @@
+module ShouldB
+  module AttributeTestHelper
+    
+    def missing_attrib(to_remove)
+      copy_of_attrib = attrib.clone
+      found = copy_of_attrib.delete to_remove
+      raise Exception, 'attribute marked for removal is missing' unless found
+      
+      return copy_of_attrib
+    end
+    
+    def blank_attrib(to_blank)
+      attrib.merge(to_blank => '')
+    end
+    
+    def replace_attrib(to_replace)
+      key = to_replace.keys.first
+      value = to_replace.values.first
+      
+      attrib.merge(key => value)
+    end
+    
+    def add_attrib(addition)
+      attrib.merge(addition)
+    end
+    
+  end
+end
--- rails/init.rb
+++ rails/init.rb
@@ -1,6 +1,8 @@
 if RAILS_ENV.eql? 'test'
   require 'test_help'
   require 'should_b'
+  require 'should_b/attribute_test_helper'
   
   ActiveSupport::TestCase.send(:include, ShouldB)
+  ActiveSupport::TestCase.send(:include, ShouldB::AttributeTestHelper)
 end
