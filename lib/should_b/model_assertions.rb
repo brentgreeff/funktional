d319ada618a3733a04220f875f1724175686b771
Brent Greeff <brentgreeff@Unknown-00-23-6c-95-2b-93.home>
2009-07-26 12:26:28 +0100

    Refactored code in model_assertions into clearer calling code, reduced some dupe.

--- lib/should_b/model_assertions.rb
+++ lib/should_b/model_assertions.rb
@@ -1,30 +1,20 @@
 module ShouldB
   module ModelAssertions
     def should_require_a(field, expected_error_message = nil)
-      if self.invalid?
-        raise ArgumentError, "ShouldB requires a valid instance of #{self.class}"
+      check_test_instance!
+      must_be_valid_before_test!
+      
+      assign_nil_to do |field, expected_error_message|
+        assert_invalid
       end
-      
-      saved_existing_value = self.send field
-      self.send "#{field}=", nil
-      
-      ShouldB.test_instance.assert self.invalid?
-      
-      if expected_error_message
-        ShouldB.test_instance.assert_equal expected_error_message, self.errors[field]
-      end
-      
-      self.send "#{field}=", saved_existing_value
-      self.valid?
     end
     
     def should_not_require_a(field)
-      saved_existing_value = self.send field
-      self.send "#{field}=", nil
+      check_test_instance!
       
-      ShouldB.test_instance.assert self.valid?
-      
-      self.send "#{field}=", saved_existing_value
+      assign_nil_to do |field|
+        assert_valid
+      end
     end
     
     def should_require_an(field, expected_error_message = nil)
@@ -36,10 +26,49 @@ module ShouldB
     end
     
     def should_have_invalid(field, expected_error_message = nil)
-      ShouldB.test_instance.assert self.invalid?
+      check_test_instance!
       
-      if expected_error_message
-        ShouldB.test_instance.assert_equal expected_error_message, self.errors[field]
+      assert_invalid
+      assert_message(field, expected_error_message) if expected_error_message
+    end
+    
+    private
+    
+    def assign_nil_to(field, expected_error_message = nil)
+      saved_existing_value = self.send field
+      self.send "#{field}=", nil
+      
+      yield
+      
+      assert_message(field, expected_error_message) if expected_error_message
+      
+      self.send "#{field}=", saved_existing_value
+      
+      # clear errors
+      self.valid?
+    end
+    
+    def assert_invalid
+      ShouldB.test_instance.assert self.invalid?
+    end
+    
+    def assert_valid
+      ShouldB.test_instance.assert self.valid?
+    end
+    
+    def assert_message(field, expected_error_message)
+      ShouldB.test_instance.assert_equal expected_error_message, self.errors[field]
+    end
+    
+    def must_be_valid_before_test!
+      if self.invalid?
+        raise ArgumentError, "ShouldB requires a valid instance of #{self.class}"
+      end
+    end
+    
+    def check_test_instance!
+      if ShouldB.test_instance.nil?
+        raise Error, 'Did you forget to (setup :should_b) in test/test_helper?'
       end
     end
   end
