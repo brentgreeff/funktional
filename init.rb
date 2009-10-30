fc4872f41e310c19d0e963028cfacd7e125a6502
Brent Greeff <brentgreeff@brent-greeffs-macbook.local>
2009-10-30 00:05:56 +0000

    Load the random_characters helper in the console.

--- init.rb
+++ init.rb
@@ -1,3 +1,8 @@
 if RAILS_ENV.eql? 'test'
   require File.join(File.dirname(__FILE__), "lib", "should_b")
 end
+
+if $0 == 'irb'
+  require 'should_b/random_characters'
+  include ShouldB::RandomCharacters
+end
