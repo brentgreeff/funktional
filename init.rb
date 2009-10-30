6fb8e0fe64e489a895386126826694446807f0ea
Brent Greeff <brentgreeff@brent-greeffs-macbook.local>
2009-10-30 00:58:16 +0000

    Fixed issue with include (sent now to Integer.)

--- init.rb
+++ init.rb
@@ -2,5 +2,7 @@ if RAILS_ENV.eql? 'test'
   require File.join(File.dirname(__FILE__), "lib", "should_b")
 end
 
-require 'should_b/random_characters'
-include ShouldB::RandomCharacters
+if RAILS_ENV.eql? 'test' or $0 == 'irb'
+  require 'should_b/random_characters'
+  Integer.send :include, ShouldB::RandomCharacters
+end
