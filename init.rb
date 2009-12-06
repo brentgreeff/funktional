6542477afbd1b9ff1bc04102cd3fb57558047827
Brent Greeff <brentgreeff@brent-greeffs-macbook.local>
2009-12-06 22:17:43 +0000

    Update load paths etc in init for move to Funktional.

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 if RAILS_ENV.eql? 'test'
-  require File.join(File.dirname(__FILE__), "lib", "should_b")
+  require File.join(File.dirname(__FILE__), "lib", "funktional")
 end
 
 if RAILS_ENV.eql? 'test' or $0 == 'irb'
-  require 'should_b/random_characters'
-  Integer.send :include, ShouldB::RandomCharacters
+  require 'funktional/random_characters'
+  Integer.send :include, Funktional::RandomCharacters
 end
