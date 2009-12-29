43068db22abbad264ad97b0dfa18b190c53eb72f
Brent Greeff <brentgreeff@brent-greeffs-macbook.local>
2009-12-29 14:27:13 +0000

    Need instance_eval as method on route_checker.

--- lib/funktional/route_checker.rb
+++ lib/funktional/route_checker.rb
@@ -1,6 +1,6 @@
 module Funktional
   class BlankSlate
-    instance_methods.each { |m| undef_method m unless m =~ /^__/ }
+    instance_methods.each { |m| undef_method m unless m =~ /^__/ or m.eql?('instance_eval') }
   end
   
   class RouteChecker < BlankSlate
