eef1dd6674ce98e060d4e4123acfa31f07cc8e8f
Brent Greeff <brentgreeff@brent-greeffs-macbook.local>
2010-02-14 01:17:39 +0000

    Fixed bug, error in should_not_block.

--- lib/funktional/context/should_not_block.rb
+++ lib/funktional/context/should_not_block.rb
@@ -3,13 +3,15 @@ module Funktional
     
     def self.build(options, context, &blk)
       
+      if options.is_a? Symbol and options.eql? :send_email
+        return ShouldNotSendEmailBlock.new(context)
+      end
+      
       case options.keys.first
         when :create
           ShouldNotCreateBlock.new(options[:create], context)
         when :delete
           ShouldNotDeleteBlock.new(options[:delete], context)
-        when :send_email
-          ShouldNotSendEmailBlock.new(context)
         else
           raise "Unknown assertion [should_not #{options.inspect}]"
       end
