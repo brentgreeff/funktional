ca7ec7c61c5f5b3b09d2f331ed3f687094b8bff5
Brent Greeff <brentgreeff@brent-greeffs-macbook.local>
2010-12-19 07:14:30 +0000

    Trying to fix load path issue in the installed gem.

--- lib/funktional.rb
+++ lib/funktional.rb
@@ -1,5 +1,5 @@
 Rails.backtrace_cleaner.add_silencer { |line| line =~ /funktional/ }
 
 if Rails.env.eql? 'test' or $0 == 'irb'
-  require 'funktional/load'
+  require File.join(File.dirname(__FILE__), "funktional", "load")
 end
